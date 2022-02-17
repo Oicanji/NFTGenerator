ae70814f07c95a1135c6eceacc0fac48718c2067
Oicanji <51388779+Oicanji@users.noreply.github.com>
2022-02-17 06:45:57 -0300

    err

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from ast import pattern
+from unicodedata import name
 from PIL import Image
 import random
 import os
@@ -28,6 +29,8 @@ def createExample():
     card.show()
 
 def main():
+    clear = lambda: os.system('cls')
+    clear()
     print("-"*50)
     print('hello world, to '+'\033[92m'+'NFT'+'\033[0m'+' generator')
     print('this is developed by '+'\033[93m'+'@Oicaji'+'\033[0m')
@@ -36,13 +39,15 @@ def main():
     menu()
 
 def menu():
-    print('options: [1] create example, [2] manual input images')
+    print('options: [0] exit, [1] create example, [2] manual input images')
     option = input('respost: ')
     print("-"*50)
     if option == '1':
         createExample()
     elif option == '2':
         manualInputs()
+    elif option == '0':
+        exit()
     else:
         print('invalid respost')
         menu()
@@ -67,12 +72,11 @@ def thisArchiveExists(name,pattern_name):
         return True
     return False
 
-def printAlredyAdded(part, pattern_added):
+def printAlredyAdded(pattern_added):
     print('patterns already added:')
     for i in pattern_added:
         print('\n -'+i['name'])
         print('\n')
-    manualMenu(part, pattern_added)
 
 def manualMenu(part, pattern_added):
         print('What image add into \033[1m'+part['name']+'\033[0m positon?')
@@ -83,7 +87,7 @@ def manualMenu(part, pattern_added):
             manualMenu(part, pattern_added)
         elif option == '..':
             if(pattern_added.__len__() != 0):
-                printAlredyAdded(part, pattern_added)
+                printAlredyAdded(pattern_added)
                 manualMenu(part, pattern_added)
             else:
                 print('\n\033[91mno patterns added yet\033[0m\n')
@@ -92,37 +96,82 @@ def manualMenu(part, pattern_added):
             option = option + '.png'
             if thisArchiveExists(option,part['name']):
                 pattern_added.append({'file': option, 'directory': part['name']})
+                to_black_list = {'file': option, 'directory': part['name']}
                 print('\n'+'\033[92m'+'pattern added'+'\033[0m\n')
-                return
+                return to_black_list
             else:
                 print('\n'+'\033[91m'+'pattern not found'+'\033[0m\n')
                 manualMenu(part, pattern_added)
 
+def isRarity(part,pattern_added):
+    if 'rarity' in part:
+        print('You can add pattern to '+'\033[1m'+part['name']+'\033[0m'+' in you image?')
+        print('options: [y] add pattern, [n] skip')
+        option = input('respost: ')
+        if option == 'y':
+            return True
+        elif option == 'n':
+            return False
+    else:
+        return True
 
+def isLinked(part,black_list):
+    if 'linked' in part:
+        if part['linked'] == 'instead':
+            print('The '+'\033[1m'+part['name']+'\033[0m'+' is linked with \033[1m'+part['parent']+'\033[0m'+' you can this?')
+            print('options: [y] continue to linked in \033[1m'+part['parent']+'\033[0m, [any] skip')
+            option = input('respost: ')
+            if option == 'y':
+                black_list.append(part['parent'])
+        elif part['closed'] == 'instead':
+            black_list.append(part['parent'])
+    return
+def blackListExist(list, name):
+    for i in list:
+        print(i)
+        if i['directory'] == name:
+            return True
+    return False
+def thisFileReallyExist(part,black_list):
+    directory_name = ''
+    file_name = ''
+    idx = -1
+    for i in black_list:
+        if i['directory'] == part['name']:
+            directory_name = i['directory']
+            file_name = i['file']
+            idx = int(i)
+    #remove de diretory name from file name
+    #and add part['name'] to file name
+    file_slice = file_name.split(directory_name)
+    file_objetive = part[name]+file_slice
+    print(file_objetive)
+    if( not os.path.exists("resource/"+file_objetive) ):
+        print('\033[91m'+'Error parent is not found')
+        print('Please insert other file in place'+'\033[0m\n')
+        #remove art['name'] from black_list
+        black_list.pop(idx)
+            
 def manualInputs():
     pattern_added = []
+    black_list = [{'directory': '-1'}]
     for part in patterns:
-        if 'rarity' in part:
-            print('You can add pattern to '+'\033[1m'+part['name']+'\033[0m'+' in you image?')
-            print('options: [y] add pattern, [n] skip')
-            option = input('respost: ')
-            print('\n')
-            if option == 'y':
-                manualMenu(part, pattern_added)
-            elif option == 'n':
-                continue
-        else:
-            manualMenu(part, pattern_added)
+        if blackListExist(black_list,part['name']):
+            thisFileReallyExist(part,black_list)
+            continue
+        if isRarity(part, pattern_added) and not blackListExist(black_list,part['name']):
+            isLinked(part,black_list)
+            respost_manualMenu = manualMenu(part, pattern_added)
+            if respost_manualMenu != None:
+                black_list.append(respost_manualMenu)
 
     createImage(pattern_added)
+    menu()
     
 def createImage(list_patterns):
     card = createBackground()
     for i in list_patterns:
         card = addImage(card,'resource/'+i['directory']+'/'+i['file'])
     card.save("result/new.png", format="png")
-    card.show()
-
-    menu()
 
 main()
--- patterns.py
+++ patterns.py
@@ -1,6 +1,18 @@
-#create rarity to for and only non-vital things 
+#create "rarity" to for and only non-vital things 
 
-hairinbackground = {'name': 'hair-in-background'}
+#if you put a "linked" one, it will link by instances files with the same name in different folders 
+#"linked" = instead ... let the user or algorithm decide
+#"linked" = closed ...  links the linked files with a rule within the system and the link name
+
+#"parent" = parent to the linked files
+#you must instantiate "parent" after the "linked", because it will be used to link the files
+#don't complicate things, don't forget the "parent" please my friend
+
+# The link is unnecessary if you have instantiated it so there is no need to instance twice you idiot 
+#that is
+#if you have already linked this pattern, do not need to link again... please 
+
+hairinbackground = {'name': 'hair-in-background', 'linked': 'instead', 'parent':'hair-in-face'}
 face = {'name': 'face'}
 bushes = {'rarity': 80, 'name': 'bushes'}
 skinmark = {'rarity': 15, 'name': 'skinmark'}
